5cfb18cf9416e97461b0fc05f10360070c0b82ba
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-12-20 13:42:20 +0100

    added filter function to books array

---
+++ readme.md
@@ -0,0 +1,10 @@
+courses and coding along videos - Mike Derycke
+
+Web2 - Group project with Rebecca Rosies & Britt Hellinckx
+    https://github.com/EHB-MCT/web-2-groupproject-frontend-team-britt-liese-rebecca.git
+
+window.location.pathname
+    https://stackoverflow.com/questions/9578348/best-way-to-execute-js-only-on-specific-page 
+
+array filter function example
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter?retiredLocale=nl
--- src/index.js
+++ src/index.js
@@ -2,25 +2,65 @@ import _ from 'lodash';
 
 "use strict"
 
-let books = {
-    init(){
-        this.getBooks()
-    },
-    getBooks() {
-        console.log("yes")
+
+//load all books in home section
+let Ownbooks = {
+    init() {
         fetch('https://web2-courseproject-liese.herokuapp.com/books')
             .then(resp => resp.json())
             .then(data => {
-                console.log(data);
-                let htmlString = ``
-                for (let i = 0; i < 7; i++) {
-                    htmlString += `<article id="${data[i]._id}">
-                <img src="https://covers.openlibrary.org/b/id/${data[i].book_id}-M.jpg" alt="" width="100px">
-            </article>`
-                }
-                document.getElementById('previewBookshelf').innerHTML = htmlString;
+                let bookshelfBooks = data.filter(data => data.wishlist == false);
+                let wishlistBooks = data.filter(data => data.wishlist == true);
+                this.getBooks(bookshelfBooks, wishlistBooks)
             })
+    },
+    getBooks(bookshelfBooks, wishlistBooks) {
+        let htmlBookshelfString = ``
+        let htmlWishlistString = ``
+        if (window.location.pathname == '/docs/html/home.html') {
+            for (let i = 0; i < 7; i++) {
+                if(bookshelfBooks[i] == undefined){
+
+                } else {
+                    htmlBookshelfString += `<article id="${bookshelfBooks[i]._id}">
+                    <img src="https://covers.openlibrary.org/b/id/${bookshelfBooks[i].book_id}-M.jpg" alt="" width="100px">
+                </article>`
+                }
+            }
+            document.getElementById('previewBookshelf').innerHTML = htmlBookshelfString;
+
+            for (let i = 0; i < 7; i++) {
+                if(wishlistBooks[i] == undefined){
+                } else {
+                    htmlWishlistString += `<article id="${wishlistBooks[i]._id}">
+                        <img src="https://covers.openlibrary.org/b/id/${wishlistBooks[i].book_id}-M.jpg" alt="" width="100px">
+                    </article>`
+                }
+                
+            }
+            document.getElementById('previewWishlist').innerHTML = htmlWishlistString;
+
+
+
+
+        } else if (window.location.pathname == '/docs/html/bookshelf.html') {
+            bookshelfBooks.forEach(x => {
+                htmlBookshelfString += `<article id="${x._id}">
+                <img src="https://covers.openlibrary.org/b/id/${x.book_id}-M.jpg" alt="" width="100px">
+            </article>`
+            });
+            document.getElementById('booksBookshelf').innerHTML = htmlBookshelfString;
+        } else if (window.location.pathname == '/docs/html/wishlist.html') {
+            wishlistBooks.forEach(x => {
+                htmlWishlistString += `<article id="${x._id}">
+                <img src="https://covers.openlibrary.org/b/id/${x.book_id}-M.jpg" alt="" width="100px">
+            </article>`
+            });
+            document.getElementById('booksWishlist').innerHTML = htmlWishlistString;
+        }
     }
 }
 
-books.init();
+if (window.location.pathname == '/docs/html/home.html' || window.location.pathname == '/docs/html/bookshelf.html' || window.location.pathname == '/docs/html/wishlist.html') {
+    Ownbooks.init();
+}
