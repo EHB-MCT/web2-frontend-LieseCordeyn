19185af6a8f4d5eb1af55e9cda5d8c720d4785d6
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-12-22 10:20:12 +0100

    function that shows all info when clicking on book

--- readme.md
+++ readme.md
@@ -1,10 +1,16 @@
 courses and coding along videos - Mike Derycke
 
-Web2 - Group project with Rebecca Rosies & Britt Hellinckx
+Web2 - Group project with Rebecca Rosies & Britt Hellinckx - index.js
     https://github.com/EHB-MCT/web-2-groupproject-frontend-team-britt-liese-rebecca.git
 
-window.location.pathname
+window.location.pathname - index.js
     https://stackoverflow.com/questions/9578348/best-way-to-execute-js-only-on-specific-page 
 
-array filter function example
+array filter function example - index.js
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter?retiredLocale=nl
+
+documentation open library API - index.js
+    https://openlibrary.org/developers/api
+
+style.display = "inherit" - index.css
+    https://www.w3schools.com/jsref/prop_style_display.asp
--- src/index.js
+++ src/index.js
@@ -59,13 +59,70 @@ let Ownbooks = {
             document.getElementById('booksWishlist').innerHTML = htmlWishlistString;
         }
     },
-    showBookInfo(id) {
+    showBookInfo(id, location) {
         console.log(id)
-        fetch(`https://web2-courseproject-liese.herokuapp.com/book?id=${id}`)
+        fetch(`https://web2-courseproject-liese.herokuapp.com/book:?id=${id}`)
             .then(resp => resp.json())
             .then(data => {
                 console.log(data)
+                let htmlString = `  <div>
+                <img src="https://covers.openlibrary.org/b/id/${data.book_id}-L.jpg" alt="">
+            </div>
+            <div>
+                <div id="mainInfo">
+                    <p>TITLE: ${data.title}</p>
+                    <p>AUTHOR: ${data.author}</p>
+                </div>
+                <div id="personalInfo">
+                    <p>STARTED: ${data.started}</p>
+                    <p>FINISHED: ${data.finished}</p>
+                    <p>PROCES: ${data.proces}</p>
+                    <p>FAVORITE CHARACTER: ${data.favorite_character}</p>
+                    <p>FAVORITE CHAPTER: ${data.favorite_chapter}</p>
+                </div>
+                <div id="extraInfo">
+                    <div id="quote">
+                        <p>FAVORITE QUOTE</p>
+                        <p>${data.favorite_quote}</p>
+                    </div>
+                    <div id="rating">
+                        <p>RATING: ${data.rating}/5</p> `
+                if (data.current_read) {
+                    htmlString += `<p>CURRENT READ: YES</p>`
+                } else {
+                    htmlString += `<p>CURRENT READ: NO</p>`
+                }
+
+                if (data.to_be_read) {
+                    htmlString += `<p>TO BE READ: YES</p>`
+                } else {
+                    htmlString += `<p>TO BE READ: NO</p>`
+                }
+
+
+               htmlString += `</div>
+                </div>
+                <div id="buttonsInfo">
+                    <p id="return">Return</p>
+                    <p id="edit">Edit</p>
+                </div>
+                </div>`
+
+                document.getElementById("bookInfoWishlist").innerHTML = htmlString;
+
+                document.getElementById("edit").addEventListener('click', (e) => {
+                    Ownbooks.editForm(id)
+                })
+
+                document.getElementById('return').addEventListener('click', (e) => {
+                    document.getElementById("bookInfoWishlist").innerHTML = "";
+                    document.getElementById(location).style.display = "inherit";
+                    document.getElementById("bookInfoWishlist").style.display = "none";
+                })
             })
+    },
+    editForm(id){
+        console.log('ready to edit')
     }
 }
 
@@ -76,7 +133,9 @@ if (window.location.pathname == '/docs/html/home.html' || window.location.pathna
 if (window.location.pathname == '/docs/html/bookshelf.html') {
     document.getElementById('booksBookshelf').addEventListener('click', (e) => {
         if (e.target.id !== "booksBookshelf") {
-            Ownbooks.showBookInfo(e.target.id)
+            document.getElementById('mainBookshelf').style.display = "none"
+            document.getElementById('bookInfoWishlist').style.display = "flex"
+            Ownbooks.showBookInfo(e.target.id, "mainBookshelf")
         }
     })
 }
@@ -84,7 +143,9 @@ if (window.location.pathname == '/docs/html/bookshelf.html') {
 if (window.location.pathname == '/docs/html/wishlist.html') {
     document.getElementById('booksWishlist').addEventListener('click', (e) => {
         if (e.target.id !== "booksWishlist") {
-            Ownbooks.showBookInfo(e.target.id)
+            document.getElementById('mainWishlist').style.display = "none"
+            document.getElementById('bookInfoWishlist').style.display = "flex"
+            Ownbooks.showBookInfo(e.target.id, "mainWishlist")
         }
     })
 }
